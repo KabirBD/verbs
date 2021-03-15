27324cf25f1faafb230f01551152d8361d9ac1eb
Md. Kabir Hosen <70894887+KabirBD@users.noreply.github.com>
2021-03-15 10:28:00 +0600

    Update sw.js
    Changed file path

--- sw.js
+++ sw.js
@@ -1,54 +1,103 @@
 
+
+
+
+
+
+
+
 //Service worker setup
 
 const cacheName = 'v1';
+
 const cacheAssets = [
-  '/',
-  'index.html',
-  'style.css',
-  'bootstrap.css',
-  'main.js',
-  'jquery.js',
-  'img/verb_512.png',
-  'bootstrap.js',
-  'VerbForms.json'
+
+  '/verbs',
+
+  '/verbs/index.html',
+
+  '/verbs/style.css',
+
+  '/verbs/bootstrap.css',
+
+  '/verbs/main.js',
+
+  '/verbs/jquery.js',
+
+  '/verbs/img/verb_512.png',
+
+  '/verbs/bootstrap.js',
+
+  '/verbs/VerbForms.json'
+
 ];
 
 // Call Install Event
+
 self.addEventListener('install', e => {
+
   console.log('Service Worker: Installed');
 
   e.waitUntil(
+
     caches
+
     .open(cacheName)
+
     .then(cache => {
+
       console.log('Service Worker: Caching Files');
+
       cache.addAll(cacheAssets);
+
     })
+
     .then(() => self.skipWaiting())
+
   );
+
 });
 
 // Call Activate Event
+
 self.addEventListener('activate', e => {
+
   console.log('Service Worker: Activated');
+
   // Remove unwanted caches
+
   e.waitUntil(
+
     caches.keys().then(cacheNames => {
+
       return Promise.all(
+
         cacheNames.map(cache => {
+
           if (cache !== cacheName) {
+
             console.log('Service Worker: Clearing Old Cache');
+
             return caches.delete(cache);
+
           }
+
         })
+
       );
+
     })
+
   );
+
 });
 
 // Call Fetch Event
+
 self.addEventListener('fetch', e => {
+
   console.log('Service Worker: Fetching');
+
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+
 });
